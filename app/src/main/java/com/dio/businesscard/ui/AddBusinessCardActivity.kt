7f9fb0cf2e42a3a6ad229eece643e4556903338c
lucas <lucaslcslcs1998@gmail.com>
2022-07-31 21:58:32 -0300

    montando layout do projeto 3

---
+++ app/src/main/java/com/dio/businesscard/ui/AddBusinessCardActivity.kt
@@ -0,0 +1,25 @@
+package com.dio.businesscard.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.dio.businesscard.databinding.ActivityAddBusinessCardBinding
+
+class AddBusinessCardActivity : AppCompatActivity() {
+
+    private val binding by lazy { ActivityAddBusinessCardBinding.inflate(layoutInflater)}
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+        insertListerners()
+    }
+
+    private fun insertListerners() {
+        binding.btnClose.setOnClickListener {
+            finish()
+        }
+        binding.btnConfirm.setOnClickListener {
+
+        }
+    }
+}
--- app/src/main/java/com/dio/businesscard/ui/MainActivity.kt
+++ app/src/main/java/com/dio/businesscard/ui/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.dio.businesscard.ui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.dio.businesscard.databinding.ActivityMainBinding
@@ -11,5 +12,13 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+        insertListener()
+    }
+
+    private fun insertListener(){
+        binding.fab.setOnClickListener {
+            val intent = Intent(this@MainActivity, AddBusinessCardActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
